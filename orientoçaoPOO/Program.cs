fba553bb43cf11ee203915a32042a1d6b8acc92d
[lince_academye] <[julianokeil@gmail.com]>
2023-10-01 17:39:30 -0300

    Csharp

--- orientoçaoPOO/Program.cs
+++ orientoçaoPOO/Program.cs
@@ -7,7 +7,9 @@ class Program
         public int candidato1 { get; set; }
         public int candidato2 { get; set; }
         public int candidato3 { get; set; }
-
+        public int candidato4 { get; set; }
+        public int candidato5 { get; set; }
+        public int candidato6 { get; set; }
     }
     
 }
