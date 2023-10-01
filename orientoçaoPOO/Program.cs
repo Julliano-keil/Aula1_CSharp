e12dd8eb79b383f8fd591c0d6323e558cab9d89c
[lince_academye] <[julianokeil@gmail.com]>
2023-10-01 17:12:27 -0300

    Csharp

---
+++ orientoçaoPOO/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    internal class Eleicao
+    {
+        public int candidato1 { get; set; }
+        public int candidato2 { get; set; }
+        public int candidato3 { get; set; }
+
+    }
+    public Eleicao()
+    {
+        joao = "[122]"
+       
+    }
+}
